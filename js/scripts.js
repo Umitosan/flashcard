3c032df40df18dabfa3627f94cd3d49f266e8b07
Dominic Brown and Ken Rutan <ken@>
2017-03-21 13:19:22 -0700

    Make and initial commit to the file which allows us to alternate displaying flashcards.

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  $("body").hover(function() {
+  });
+
+  $(".clickable1").hover(function() {
+    $(".card-showing1").fadeToggle(500);
+    $(".card-hidden1").fadeToggle(500);
+  });
+
+  $(".clickable2").hover(function() {
+    $(".card-showing2").fadeToggle(500);
+    $(".card-hidden2").fadeToggle(500);
+  });
+
+  $(".clickable3").hover(function() {
+    $(".card-showing3").fadeToggle(500);
+    $(".card-hidden3").fadeToggle(500);
+  });
+
+  $(".clickable4").hover(function() {
+    $(".card-showing4").fadeToggle(500);
+    $(".card-hidden4").fadeToggle(500);
+  });
+
+  $(".clickable5").hover(function() {
+    $(".card-showing5").fadeToggle(500);
+    $(".card-hidden5").fadeToggle(500);
+  });
+
+  $(".clickable6").hover(function() {
+    $(".card-showing6").fadeToggle(500);
+    $(".card-hidden6").fadeToggle(500);
+  });
+
+  $(".clickable7").hover(function() {
+    $(".card-showing7").fadeToggle(500);
+    $(".card-hidden7").fadeToggle(500);
+  });
+
+  $(".clickable8").hover(function() {
+    $(".card-showing8").fadeToggle(500);
+    $(".card-hidden8").fadeToggle(500);
+  });
+
+});
